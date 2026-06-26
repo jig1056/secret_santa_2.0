fef363409291079bc1d2fae12eccf1b575e83178
Mark Nelson <mnelson23@gmail.com>
2026-06-26 17:56:37 -0400

    Just
    testing

--- www/secret_santa_2.0/dev/pages/home.php
+++ www/secret_santa_2.0/dev/pages/home.php
@@ -1,7 +1,7 @@
 <?php
 // ============================================================
 // home.php
-// Role-aware dashboard.
+// Role-aware dashboard..
 // ============================================================
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../includes/db.php';
